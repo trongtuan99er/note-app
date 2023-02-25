716b11dde7c60e8bd48950e05fe3e734b351318a
trongtuan99er <trongtuan681@gmail.com>
2023-02-25 20:21:48 +0700

    connect to mongodb

--- server/server.mjs
+++ server/server.mjs
@@ -6,7 +6,8 @@ import bodyParser from 'body-parser'
 import { expressMiddleware } from '@apollo/server/express4'
 import cors from 'cors'
 import fakeData from './fakeData/index.js';
-
+import mongoose from 'mongoose'
+import 'dotenv/config'
 const app = express();
 const httpServer = http.createServer(app);
 
@@ -56,7 +57,9 @@ const resolvers = {
     }
   },
 };
-
+// conect to mongodb
+const URI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.x0o6orf.mongodb.net/?retryWrites=true&w=majority`
+const PORT = process.env.PORT || 4000
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -66,7 +69,13 @@ const server = new ApolloServer({
 await server.start();
 
 app.use(cors(), bodyParser.json(), expressMiddleware(server))
+mongoose.set('strictQuery', false)
+mongoose.connect(URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(async () => {
+  console.log("conected to DB");
+  await new Promise((resolve) => httpServer.listen({port: PORT}, resolve) );
+  console.log('sever ready');
+})
 
-await new Promise((resolve) => httpServer.listen({port: 4000}, resolve) );
-
-console.log('sever ready');
